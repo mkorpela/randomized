dff1782ac290f4790679a03328fef773680af405
Mikko Korpela <mikko.korpela@gmail.com>
2017-07-27 18:50:26 +0300

    prict

--- contracts/Randomized.sol
+++ contracts/Randomized.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.4.4;
 contract Randomized {
 
     address public owner;
+    uint public price;
 
     function Randomized() {
         owner = msg.sender;
+        price = 10;
     }
 
     struct Key {
@@ -15,18 +17,19 @@ contract Randomized {
 
     mapping (address => Key) keys;
 
-    function setKey(bytes publicKey) {
+    function setKey(bytes publicKey) payable public {
+        require(msg.value >= price);
         keys[msg.sender].entryBlockNumber = block.number;
         keys[msg.sender].publicKey = publicKey;
     }
 
-    function validate(uint seedBlockNumber, bytes seed, address sender, bytes32 crypted, bytes32 result) constant returns (bool) {
+    function validate(uint seedBlockNumber, bytes seed, address sender, bytes32 crypted, bytes32 result) constant public returns (bool) {
         if (keys[sender].entryBlockNumber >= seedBlockNumber) return false;
         if (keccak256(crypted, seed) != result) return false;
-        return keccak256(seed) == this.privatized(crypted, keys[sender].publicKey);
+        return keccak256(seed) == privatized(crypted, keys[sender].publicKey);
     }
 
-    function privatized(bytes32 crypted, bytes publicKey) constant returns (bytes32) {
+    function privatized(bytes32 crypted, bytes publicKey) constant private returns (bytes32) {
         // Waiting for https://github.com/ethereum/EIPs/pull/198
         return 123;
     }
